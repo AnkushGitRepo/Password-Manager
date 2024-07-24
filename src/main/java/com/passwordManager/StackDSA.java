966ae722fcefd1e8e6946dda025789cd0b27399a
Ankush Gupta <70625252+AnkushGitRepo@users.noreply.github.com>
2024-07-24 16:31:07 +0530

    Create StackDSA.java

---
+++ src/main/java/com/passwordManager/StackDSA.java
@@ -0,0 +1,44 @@
+package com.passwordManager;
+
+import java.util.EmptyStackException;
+
+public class StackDSA<T> {
+    private Node<T> top;
+    private int size;
+
+    public StackDSA() {
+        top = null;
+        size = 0;
+    }
+
+    public void push(T data) {
+        Node<T> newNode = new Node<>(data);
+        newNode.next = top;
+        top = newNode;
+        size++;
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        T data = top.data;
+        top = top.next;
+        size--;
+        return data;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+}
