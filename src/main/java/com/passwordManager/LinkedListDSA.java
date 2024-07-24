f021734c9620dccaa8dbeb1361571da80a7ac874
Ankush Gupta <70625252+AnkushGitRepo@users.noreply.github.com>
2024-07-24 16:28:56 +0530

    Update LinkedListDSA.java

--- src/main/java/com/passwordManager/LinkedListDSA.java
+++ src/main/java/com/passwordManager/LinkedListDSA.java
@@ -1,27 +1,72 @@
 package com.passwordManager;
 
-public class LinkedListDSA {
-    private User head;
+public class LinkedListDSA<T> {
+    private Node<T> head;
 
-    public void addUser(User user) {
+    // Add data to the end of the list
+    public void add(T data) {
         if (head == null) {
-            head = user;
+            head = new Node<>(data);
         } else {
-            User current = head;
-            while (current.getNext() != null) {
-                current = current.getNext();
+            Node<T> current = head;
+            while (current.next != null) {
+                current = current.next;
             }
-            current.setNext(user);
+            current.next = new Node<>(data);
         }
     }
 
-    public User findUserByEmail(String email) {
-        User current = head;
+    // Find if the list contains the specified data
+    public boolean contains(T data) {
+        Node<T> current = head;
         while (current != null) {
-            if (current.getEmail().equals(email)) {
-                return current;
+            if (current.data.equals(data)) {
+                return true;
             }
-            current = current.getNext();
+            current = current.next;
+        }
+        return false;
+    }
+
+    // Remove specified data from the list
+    public boolean remove(T data) {
+        if (head == null) {
+            return false;
+        }
+        if (head.data.equals(data)) {
+            head = head.next;
+            return true;
+        }
+        Node<T> current = head;
+        while (current.next != null) {
+            if (current.next.data.equals(data)) {
+                current.next = current.next.next;
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+    // Inner Node class
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // Additional method to find user by email
+    public T findUserByEmail(String email) {
+        Node<T> current = head;
+        while (current != null) {
+            if (((User) current.data).getEmail().equals(email)) {
+                return current.data;
+            }
+            current = current.next;
         }
         return null;
     }
